4183a8816a1983af0ed3e5eaf8e7730fcb377cc9
marcaurelsouaga <msouaga3@gmail.com>
2026-07-07 17:06:54 +0000

    Remise à zéro config SQLite

--- config/database.php
+++ config/database.php
@@ -31,14 +31,18 @@ return [
     */
 
     'connections' => [
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => database_path('database.sqlite'), // <- CETTE LIGNE FORCE LE CHEMIN ABSOLU ULTIME
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+            'busy_timeout' => null,
+            'journal_mode' => null,
+            'synchronous' => null,
+            'transaction_mode' => 'DEFERRED',
         ],
-
 
         'mysql' => [
             'driver' => 'mysql',
@@ -145,7 +149,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
