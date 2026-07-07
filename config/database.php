73e7aa7a455edb1f55d2b70a21c916f9cd9fee99
marcaurelsouaga <msouaga3@gmail.com>
2026-07-07 17:00:21 +0000

    Fix ultime chemin SQLite

--- config/database.php
+++ config/database.php
@@ -31,18 +31,14 @@ return [
     */
 
     'connections' => [
-
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => database_path('database.sqlite'), // <- CETTE LIGNE FORCE LE CHEMIN ABSOLU ULTIME
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-            'busy_timeout' => null,
-            'journal_mode' => null,
-            'synchronous' => null,
-            'transaction_mode' => 'DEFERRED',
         ],
+
 
         'mysql' => [
             'driver' => 'mysql',
@@ -149,7 +145,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
